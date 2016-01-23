1391ea24a0994c9f78d640663f97518f852ccb40
Alexis <leonidas101@live.com.mx>
2016-01-23 00:41:17 -0600

    prueba 2 vista curso

--- PEducativa/resources/views/profesor/irCurso.blade.php
+++ PEducativa/resources/views/profesor/irCurso.blade.php
@@ -1,1 +1,17 @@
-hola
+@extends('profesor.perfil')
+{!! Html::script('bower_components/jquery/dist/jquery.min.js')!!}
+{!! Html::script('scripts/cursos.js')!!}
+
+{!! Html::style('bower_components/bootstrap-material-design-icons/css/material-icons.css') !!}
+{!! Html::style('css/adaptaciones.css') !!}
+
+
+@section('content')
+<div class="jumbotron jumbotron-fluid">
+<h1 class="display-3">Curso seleccionado</h1>
+<br>
+<p class="lead"> Detalles del curso seleccionado
+</div>
+
+
+@endsection

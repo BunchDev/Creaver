dd85035c08477a9143fb2bdebdeaaa68893e2bc9
Cristian Michel <cristianrocker93@gmail.com>
2016-01-31 17:37:16 -0600

    fix

--- PEducativa/app/Http/routes.php
+++ PEducativa/app/Http/routes.php
@@ -27,11 +27,9 @@ Route::get('cursos', 'CursosController@listarCursos');
 Route::post('crearCurso', 'CursosController@store');
 Route::post('crearActividad', 'ActividadController@store');
 Route::post('irCurso/{id}', 'CursosController@irCurso');
-<<<<<<< HEAD
-Route::post('irCursoAprobar/{id}', 'CursosController@irCursoAprobar');
-=======
 
->>>>>>> origin/master
+Route::post('irCursoAprobar/{id}', 'CursosController@irCursoAprobar');
+
 /* ----------------- PERFIL  ------------------*/
 
 Route::get('perfil',function(){
